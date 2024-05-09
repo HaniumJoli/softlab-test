1ccc6b087844ed8d9a970939a40813c2a548011a
--replace-all <haniumjoli1015@gmail.com>
2024-05-09 13:39:02 +0600

    first

---
+++ app/src/main/java/com/arf/myapplication/color.java
@@ -0,0 +1,16 @@
+package com.arf.myapplication;
+
+public class color {
+    private String color;
+    public color(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
